54a6613ada2025af9fbfddd4dd5463518e9c8244
Zain Haider Naqvi <syedzainnaqvi3324@gmail.com>
2022-07-20 23:25:22 +0500

    Added BottomNavigation bar

--- lib/screens/mainpage.dart
+++ lib/screens/mainpage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_application/screens/food_home_page/food_home_page.dart';
+import 'package:get/get.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -9,10 +10,56 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  var selectedPage = 0;
+  final List _pages = [
+    FoodHomePage(),
+    Center(
+      child: Text("Container"),
+    ),
+    Center(
+      child: Text("Container"),
+    ),
+    Center(
+      child: Text("Container"),
+    )
+  ];
+  onTap(int index) {
+    setState(() {
+      selectedPage = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FoodHomePage(),
+      body: _pages[selectedPage],
+      bottomNavigationBar: BottomNavigationBar(
+          selectedIconTheme: IconThemeData(color: Colors.teal),
+          unselectedIconTheme: IconThemeData(color: Colors.teal[100]),
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          selectedFontSize: 0.0,
+          unselectedFontSize: 0.0,
+          onTap: onTap,
+          currentIndex: selectedPage,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.archive_outlined),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history_outlined),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart_outlined),
+              label: "",
+            ),
+          ]),
     );
   }
 }
